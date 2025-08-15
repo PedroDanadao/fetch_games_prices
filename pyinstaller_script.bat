ebb2153b322c8901b3c31de4061bc56f9438a3c0
PedroDanadao <15200544+PedroDanadao@users.noreply.github.com>
2025-08-15 15:39:12 -0300

    Add .bat and .sh files for generating executables

---
+++ pyinstaller_script.bat
@@ -0,0 +1,2 @@
+pyinstaller --onefile --windowed current_prices_ui.py
+pyinstaller --onefile --windowed set_games_to_check_json.py
---
+++ pyinstaller_script.sh
@@ -0,0 +1,2 @@
+python3 -m pyinstaller --onefile --windowed current_prices_ui.py
+python3 -m pyinstaller --onefile --windowed set_games_to_check_json.py
