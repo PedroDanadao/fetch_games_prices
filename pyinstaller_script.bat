15f4913193a556a56d2b879236eb78d22f18548b
PedroDanadao <15200544+PedroDanadao@users.noreply.github.com>
2025-08-17 18:29:42 -0300

    Update the pyinstaller bat file
    Update the bat so it includes the console programs in it.

--- pyinstaller_script.bat
+++ pyinstaller_script.bat
@@ -1,2 +1,4 @@
 pyinstaller --onefile --windowed current_prices_ui.py
 pyinstaller --onefile --windowed set_games_to_check_json.py
+pyinstaller --onefile --windowed current_console_prices_ui.py
+pyinstaller --onefile --windowed set_games_to_check_console_ui.py
