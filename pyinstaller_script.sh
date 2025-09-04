4787629a5eb5e9d87ab74c723d8d37b6497364db
PedroDanadao <15200544+PedroDanadao@users.noreply.github.com>
2025-09-04 14:06:33 -0300

    Update the pyinstaller_script.sh to include main_ui.py

--- pyinstaller_script.sh
+++ pyinstaller_script.sh
@@ -1,2 +1,3 @@
+python3 -m pyinstaller --onefile --windowed main_ui.py
 python3 -m pyinstaller --onefile --windowed current_prices_ui.py
 python3 -m pyinstaller --onefile --windowed set_games_to_check_json.py
